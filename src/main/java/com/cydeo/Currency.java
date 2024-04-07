336cfce460d936980db1d9c14cdd281d3e921e55
Ferhat <parhat002@gmail.com>
2024-04-07 11:07:25 +0300

    Completed

--- src/main/java/com/cydeo/Currency.java
+++ src/main/java/com/cydeo/Currency.java
@@ -1,10 +1,14 @@
 package com.cydeo;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Currency {
 
     private String code;
